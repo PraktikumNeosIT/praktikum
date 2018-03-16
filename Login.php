e4c36733c9bfec98f80b1ebb0bc22ee12c5cc29a
sky8570 <nico.josch@gmail.com>
2018-03-16 12:19:15 +0100

    asd

--- Login.php
+++ Login.php
@@ -1,79 +1,76 @@
+<?php 
+
+    session_start();
+    $pdo = new PDO('mysql:host=localhost;dbname=test', 'root', 'root');
+
+    if(isset($_GET['logout'])) {
+        $_SESSION['angemeldet'] = false;
+    }
+
+    if(isset($_GET['login'])) {
+        $email = $_POST['email'];
+        $passwort = $_POST['passwort'];
+   
+
+        $statement1 = $pdo->prepare("SELECT * FROM test WHERE email = :email_in and passwort = :passwort_in");
+        $statement1->execute(array(":email_in" => $email, ":passwort_in" => $passwort));
+
+        $user = $statement1->fetch();
+
+        if ($user === false) {
+            $errorMessage = "E-Mail oder Passwort war ungültig<br><br>";
+            $_SESSION['angemeldet'] = false;
+            unset($_SESSION['vorname']);
+        } else {
+            $_SESSION['angemeldet'] = true;
+            $_SESSION['vorname'] = $user['vorname'];
+        }
+
+
+    }
+
+?>
 <!DOCTYPE html> 
 <html> 
 <head>
-  <title>Login</title>    
+  <title>Login-PHP</title>
 </head> 
 <body>
  
 <?php 
-session_start();
-
-$erlaubteBenutzer = array(/* key => benutzername, value => passwort */
-    'nico' => '1',
-    'finn' => 'Hallo'
-);
- 
-if(isset($_GET['login'])) {
-    $email = $_POST['email'];
-    $passwort = $_POST['passwort'];
-}
-
 if(isset($errorMessage)) {
     echo $errorMessage;
 }
+    $_SESSION['zaehler'] = false;
 
-if(isset($_POST['email'])) {
-    print_r ($_POST['email']);
-    if ($_POST['email'] == 'nico') {
+if (isset($_SESSION['angemeldet']) AND ($_SESSION['angemeldet'] == true)) {
 
-}
-}
+    echo "<p>Hallo " . $_SESSION['vorname'] . ". Du bist angemeldet.</p>";
 
-var_dump ($_POST);
+    $sql = "SELECT * FROM test";
+    foreach ($pdo->query($sql) as $row) {
+    echo 'Email: <a href="user.php?id=' . $row['id'] . '">'. $row['email'] .  '</a><br>';
+    echo "Passwort: ".$row['passwort']." <br><br>";
 
-if ($email == 'nico'){
-    $hatGueltigesLogin = true;
-} 
+    }    
 
-if ($hatGueltigesLogin == true) {
-var_dump ($hatGueltigesLogin);
-    die("Angemeldet");
-}
-
-
+} else {
 
 ?>
 
 <form action="?login=1" method="post">
 E-Mail:<br>
-<input type="text" size="40" maxlength="250" name="email"><br><br>
-
+<input type="email" size="40" maxlength="250" name="email"><br><br>
+ 
 Dein Passwort:<br>
 <input type="password" size="40"  maxlength="250" name="passwort"><br>
-
+ 
 <input type="submit" value="Abschicken">
 </form> 
+
+<?php
+}
+
+?>
 </body>
 </html>
-
-
-
-|-------------------------|
-|                         |
-|                         |
-|                         |
-|                         |
-|                         |
-|                         |
-|                         |
-|                         |
-|                         |
-|                         |
-|                   ___   |
-|                         |
-|                         |
-|                         |
-|                         |
-|                         |
-|                         |
-|                         | 
--- user.php
+++ user.php
@@ -11,12 +11,8 @@ $id = $_GET ['id'];
         $statement1->execute(array(":id_in" => $id));
         $row = $id = $statement1->fetch();
 
-
-        if ($id === "3"); {
-            echo "ID: ";
-            echo $row[0].' <br>Email: '.$row[1].' <br>Passwort: '.$row[2].' <br>Vorname: '.$row[3].' <br>Nachname: '.$row[4].'<br />';
-            echo "<br>";
-
-}
+     echo "ID: ";
+     echo $row[0].' <br>Email: '.$row[1].' <br>Passwort: '.$row[2].' <br>Vorname: '.$row[3].' <br>Nachname: '.$row[4].'<br />';
+     echo "<br>";
 
 ?>
